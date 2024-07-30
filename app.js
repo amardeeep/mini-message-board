940a43f36c992128bd0ebb2209874f85d50c3f54
Deetsy <amardeepdeepak2016@gmail.com>
2024-07-30 12:34:58 +0530

    add basic style

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const indexRouter = require("./routes/indexRouter");
 
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/", indexRouter);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.use("/", indexRouter);
+
 app.listen(3000, () => {
   console.log(`Listening on port ${3000}`);
 });
