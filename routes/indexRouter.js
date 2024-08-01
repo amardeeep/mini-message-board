96cc808cc2b9a07581b7510573e53ce9c4d2b12a
Deetsy <amardeepdeepak2016@gmail.com>
2024-08-01 13:27:58 +0530

    add go back button and style to opencss

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,6 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
 indexRouter.get("/", indexController.renderIndex);
-indexRouter.get("/:title");
 indexRouter.get("/new", indexController.getForm);
 indexRouter.post("/new", indexController.postForm);
 indexRouter.get("/:title/:message/:date", indexController.getOpen);
